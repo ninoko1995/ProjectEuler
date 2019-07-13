0df8ded45661d1ed8042e085c71c23d0fbaf5b97
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-07-13 18:02:04 +0900

    finish question-16

--- Q16/Q16.java
+++ Q16/Q16.java
@@ -1,14 +1,21 @@
 import java.math.BigInteger;
-public class Q16 {
-    public static void main(String[] args) {
-        BigInteger  power = new BigInteger("2");
-        int res = 0;
-        for(int i=0;i<200;i++){
-        	power = power.multiply(power);
-        }
 
-        System.out.println(power.length());
-        
-        // System.out.println(sum);  
-    }
+public class Q16 {
+	public static void main(String[] args) {
+		final int NUM = 1000;
+		BigInteger power = new BigInteger("1");
+		int sum = 0;
+
+		for (int i = 0; i < NUM; i++) {
+			power = power.multiply(BigInteger.valueOf(2));
+		}
+
+		String digit = power.toString();
+		int length = digit.length();
+		for (int i = 0; i < length; i++) {
+			sum += Integer.parseInt(digit.substring(i, i + 1));
+		}
+		
+		System.out.println(sum);
+	}
 }
