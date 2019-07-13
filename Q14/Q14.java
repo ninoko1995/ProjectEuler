f661226b7edde62680266a1fdfdb6f4d98de1ae0
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-07-13 18:07:29 +0900

    start question-14

---
+++ Q14/Q14.java
@@ -0,0 +1,17 @@
+public class 14 {
+  public static void main(String[] args) {
+    final int LIMIT = 1000;
+
+    int sum = 0;
+    int i = 1;
+    
+    while(i<LIMIT){
+        if(i%3==0 || i%5==0){
+            sum+=i;
+        }
+        i++;	    	
+    }
+    
+    System.out.println(sum);
+  }
+}
