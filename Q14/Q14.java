c588481558c01256a117d2bdaf978438b22997b4
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-07-14 19:22:21 +0900

    refactaring to speed up

--- Q14/Q14.java
+++ Q14/Q14.java
@@ -1,37 +1,43 @@
 public class Q14 {
+
+    public static int countChain(long number, int[] chainNum) {
+        int chain = 0;
+        if (number < chainNum.length && chainNum[(int) number] > 0) {
+                return chainNum[(int) number];
+        } else {
+            if (number % 2 == 0) {
+                chain = countChain(number/2, chainNum) + 1;
+                if (number < chainNum.length) chainNum[(int) number] = chain;
+            } else {
+                chain = countChain((number * 3 + 1) / 2, chainNum) + 2;
+                if (number < chainNum.length) chainNum[(int) number] = chain;
+            }
+            return chain;
+        }
+    }
+
     public static void main(String[] args) {
         final int LIMIT = 1000000;
+        int[] chainNum = new int[LIMIT + 1];
+        chainNum[1] = 1;
 
         // 最大値記録用
         int maxChain = 0;
         int maxChainNum = 0;
 
         // LIMIT以下のすべての数値のチェーンを数え、最大値記録。
-        for (int i = 2; i <= LIMIT; i++) {
-            int times = 0;
-            long number = i;
-
+        for (int i = LIMIT / 2; i <= LIMIT; i++) {
+            
             // チェーン数える部分
-            while (number >= 1) {
-                if (number % 2 == 0) {
-                    number /= 2;
-                } else {
-                    number = number * 3 + 1;
-                }
-                times++;
-
-                if (number == 1) {
-                    break;
-                }
-
-            }
+            chainNum[i] = countChain((long) i, chainNum);
 
             // 記録部分
-            if (times + 1 > maxChain) {
-                maxChain = times + 1;
+            if (chainNum[i] > maxChain) {
+                maxChain = chainNum[i];
                 maxChainNum = i;
             }
         }
+
         System.out.println(maxChainNum);
     }
 }
