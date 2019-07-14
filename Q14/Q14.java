da20767813090bad67266a3c03c102e2c31397b0
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-07-14 18:15:24 +0900

    finish question-14

--- Q14/Q14.java
+++ Q14/Q14.java
@@ -1,17 +1,37 @@
-public class 14 {
-  public static void main(String[] args) {
-    final int LIMIT = 1000;
+public class Q14 {
+    public static void main(String[] args) {
+        final int LIMIT = 1000000;
 
-    int sum = 0;
-    int i = 1;
-    
-    while(i<LIMIT){
-        if(i%3==0 || i%5==0){
-            sum+=i;
+        // 最大値記録用
+        int maxChain = 0;
+        int maxChainNum = 0;
+
+        // LIMIT以下のすべての数値のチェーンを数え、最大値記録。
+        for (int i = 2; i <= LIMIT; i++) {
+            int times = 0;
+            long number = i;
+
+            // チェーン数える部分
+            while (number >= 1) {
+                if (number % 2 == 0) {
+                    number /= 2;
+                } else {
+                    number = number * 3 + 1;
+                }
+                times++;
+
+                if (number == 1) {
+                    break;
+                }
+
+            }
+
+            // 記録部分
+            if (times + 1 > maxChain) {
+                maxChain = times + 1;
+                maxChainNum = i;
+            }
         }
-        i++;	    	
+        System.out.println(maxChainNum);
     }
-    
-    System.out.println(sum);
-  }
 }
