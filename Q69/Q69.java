d8e431dd04c10ee8bb1ef59e0fa34aaaad14d63c
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-07-25 23:03:16 +0900

    modify to  speed up

--- Q69/Q69.java
+++ Q69/Q69.java
@@ -24,19 +24,38 @@ public class Q69 {
 
   public static void main(String[] args) {
     final int LIMIT = 1000000;
-    double max = 0;
-    int maxIndex = 0;
-
-    for(int i = 0; i < LIMIT;i ++){
-        double totient = getTotient(i);
-        if(max < totient) {
-            max = totient;
-            maxIndex = i;
-        }
-        if(i % 10000 == 0) System.out.println(i);
+    
+    // double max = 0;
+    // int maxIndex = 0;
+    // for(int i = 0; i < LIMIT;i ++){
+    //     double totient = getTotient(i);
+    //     if(max < totient) {
+    //         max = totient;
+    //         maxIndex = i;
+    //     }
+    //     if(i % 10000 == 0) System.out.println(i);
         
+    // }
+
+    // System.out.println(maxIndex);
+
+    int product = 1;    
+    int[] primeList = {
+        2,3,5,7,11,13,17,19,23,29,31
+    };
+
+    for (int i = 0;i < primeList.length;i ++) {
+        product *= primeList[i];
+        if(product > LIMIT) {
+            product /= primeList[i];
+            break;
+        }
     }
 
-    System.out.println(maxIndex);
+    System.out.println(product);
+
+
+
+    
   }
 }
