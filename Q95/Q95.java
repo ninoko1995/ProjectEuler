5445cc86de45709f0287b1588356f280e028d98d
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-08-14 22:10:42 +0900

    use LinkedList

--- Q95/Q95.java
+++ Q95/Q95.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
 public class Q95 {
     // 配列の最後に数字を一つ追加する
     public static int[] addNumToArray(int[] array, int n) {
@@ -11,10 +14,10 @@ public class Q95 {
     }
 
     // 配列の最小値を取得する
-    public static int getMiniNumOfArray(int[] array) {
+    public static int getMiniNumOfArray(int n, int[] array) {
         int length = array.length;
-        int mini = array[0];
-        for (int i = 1; i < length; i++) {
+        int mini = array[n];
+        for (int i = n; i < length; i++) {
             if (mini > array[i])
                 mini = array[i];
         }
@@ -48,33 +51,61 @@ public class Q95 {
         return (sum - n);
     }
 
+
     public static int[] countChain(int n, int[] list, int[] routes) {
         int[] result = {0, 0};//{チェーンの長さ、最小値}
+        int length = routes.length;
         list[n] = getSumOfDivisors(n);
 
         // すでに出た値に戻ったら、計算終了して記録
-        boolean finish = false;
-        for (int i = 0; i < routes.length; i++) {
+        for (int i = 0; i < length; i++) {
             if (routes[i] == list[n]) {
-                finish = true;
+                result[0] = length - 1 - i;;
+                result[1] = getMiniNumOfArray(i, routes);
                 break;
             }
         }
 
-        if (finish) {
-            result[0] = routes.length;
-            result[1] = getMiniNumOfArray(routes);
-        } else {
-            if (list[n] < list.length && list[list[n]] == 0) {
+        if (result[0] == 0 && list[n] < list.length && list[list[n]] == 0) {
             // 計算結果が最大値以下かつ次の値が未計算だった場合は、約数の総和を計算してチェーン継続
-                result = countChain(list[n], list, addNumToArray(routes,list[n]));
+            result = countChain(list[n], list, addNumToArray(routes,list[n]));
+        }
+        return result;
+    }
+
+    public static int getMiniNumOfList(int n, LinkedList<Integer> routes) {
+        int miniNum = routes.get(n);
+        int i = 0;
+        for (int value : routes) {
+            if (i > n && miniNum > value)
+                miniNum = value;
+            i++;
+        }
+        return miniNum;
+    }
+
+
+    public static int[] countChain1(int n, int[] list, LinkedList<Integer> routes) {
+        int[] result = {0, 0};//{チェーンの長さ、最小値}
+        list[n] = getSumOfDivisors(n);
+
+        // すでに出た値に戻ったら、計算終了して記録
+        int getIndex = routes.lastIndexOf(list[n]);
+        routes.addLast(list[n]);
+        if (getIndex != -1) {
+            result[0] = routes.lastIndexOf(list[n]) - getIndex;
+            result[1] = getMiniNumOfList(getIndex,routes);
+        } else {
+            // 計算結果が最大値以下かつ次の値が未計算だった場合は、約数の総和を計算してチェーン継続
+            if (list[n] < list.length && list[list[n]] == 0) {
+                result = countChain1(list[n], list, routes);
             }
         }
         return result;
     }
 
-    public static void main(String[] args) {
-        final int LIMIT = 1000000;
+
+    public static int useArrayMethod(int LIMIT) {
         // 約数の総和記録用配列
         int[] sumOfDivisors = new int[LIMIT + 1];
         // 初期化
@@ -82,6 +113,7 @@ public class Q95 {
         sumOfDivisors[1] = 1;
         sumOfDivisors[2] = 1;
         sumOfDivisors[3] = 1;
+
 
         // 最大値記録用
         int longestChain = 0;
@@ -108,5 +140,84 @@ public class Q95 {
             }
         }
         System.out.println(miniNumOfLongestChain);
+        return miniNumOfLongestChain;
+    }
+
+
+    public static int useInterfaceMethod(int LIMIT) {
+        // 約数の総和記録用配列
+        int[] sumOfDivisors = new int[LIMIT + 1];
+        // 初期化
+        sumOfDivisors[0] = 1;
+        sumOfDivisors[1] = 1;
+        sumOfDivisors[2] = 1;
+        sumOfDivisors[3] = 1;
+
+
+        // 最大値記録用
+        int longestChain = 0;
+        int miniNumOfLongestChain = LIMIT;
+
+        // LIMIT以下のすべての数に対し、それぞれ約数の和を数え、最大チェーンを取得
+        for (int i = LIMIT; i > 3; i--) {
+
+            // 進捗観察用 
+            if (i % 10000 == 0)
+                System.out.println(i);
+            
+            
+            // あるところの約数の総和を求め、その約数の総和の約数の総和が求められていない場合、求める。その場合、次の数字を連鎖的に計算して約数総和記録用配列に数字を埋めていく。。
+            if (sumOfDivisors[i] == 0) {
+                LinkedList<Integer> list = new LinkedList<Integer>();
+                list.addLast(i);
+                int[] result = countChain1(i, sumOfDivisors, list);
+                if (result[0] != 0) {
+                    if (longestChain < result[0]) {
+                        longestChain = result[0];
+                        miniNumOfLongestChain = result[1];
+                        System.out.println(i+" "+longestChain+" "+miniNumOfLongestChain);
+                    }
+                }
+            }
+        }
+        System.out.println(miniNumOfLongestChain);
+        return miniNumOfLongestChain;
+    }
+
+    public static void main(String[] args) {
+        final int LIMIT = 1000000;
+
+        // 開始時間をミリ秒で取得
+        long ts1 = System.currentTimeMillis();
+        // 処理実行
+        int ans1 = useInterfaceMethod(LIMIT);
+        // 終了時間をミリ秒で取得
+        long te1 = System.currentTimeMillis();
+        // 処理時間 ミリ秒
+        long tmsec1 = te1 - ts1;
+        // 処理時間 秒
+        double tsec1 = (double) tmsec1 / 1000.0;
+        System.out.println("処理時間 : " + tmsec1 + "ミリ秒   " + tsec1 + "秒");
+        
+
+        // 開始時間をミリ秒で取得
+        long ts2 = System.currentTimeMillis();
+        // 処理実行
+        int ans2 = useArrayMethod(LIMIT);
+        // 終了時間をミリ秒で取得
+        long te2 = System.currentTimeMillis();
+        // 処理時間 ミリ秒
+        long tmsec2 = te2 - ts2;
+        // 処理時間 秒
+        double tsec2 = (double) tmsec2 / 1000.0;
+        System.out.println("処理時間 : " + tmsec2 + "ミリ秒   " + tsec2 + "秒");
+
+        if (ans2 - ans1 != 0) {
+            System.out.println("Answers are different. Somthing wrong happened.");
+        } else {
+            System.out.println("Answer is " + ans1);
+            System.out.println("処理時間 : " + tmsec1 + "ミリ秒   " + tsec1 + "秒");
+            System.out.println("処理時間 : " + tmsec2 + "ミリ秒   " + tsec2 + "秒");
+        }
     }
 }
